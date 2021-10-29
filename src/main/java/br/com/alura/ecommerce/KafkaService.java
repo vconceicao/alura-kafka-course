3a5ad0ec631f1690a9a4987f7c6a16dc7f891e02
Vinicius Luiz Araujo da Conceicao <vlconceicao@rd.com.br>
2021-10-29 12:46:44 -0300

    class that encapsulates the consume of records

--- src/main/java/br/com/alura/ecommerce/KafkaService.java
+++ src/main/java/br/com/alura/ecommerce/KafkaService.java
@@ -1,2 +1,56 @@
-package br.com.alura.ecommerce;public class KafkaService {
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class KafkaService {
+    private final ConsumerFunction parser;
+    private final KafkaConsumer<String, String> consumer;
+
+    public KafkaService(String topic, ConsumerFunction parser) {
+        this.parser = parser;
+        this.consumer =  new KafkaConsumer<String, String>(properties());
+        //subscribing to a topic
+        consumer.subscribe(Collections.singletonList(topic));
+
+        //Infinite loop to keep checking for new messeges
+
+    }
+
+    private static Properties properties() {
+        final var properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
+
+        return properties;
+    }
+
+    public void run() {
+        while (true) {
+
+
+            //asking to consumer if theres new register in the topic
+            final ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            //if there`s registers proccess them
+            if (!records.isEmpty()) {
+                System.out.println("Registers found");
+                records.forEach(r -> {
+
+                    this.parser.consume(r);
+
+                });
+            }
+
+        }
+
+    }
 }
