b68ed264c4a7e4cd62f494a7db5b97d7c5facc94
Vinicius Luiz Araujo da Conceicao <vlconceicao@rd.com.br>
2021-10-30 16:48:25 -0300

    refactored new order main to use kafka dispatcher class

--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -19,48 +19,18 @@ public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        //create a kafka producer
-        final var kafkaProducer = new KafkaProducer<String, String>(properties());
 
-        for (var i =0; i<100; i++) {
-            //creating a new id for the user, this is because kafka will use this info as key to parallelize the messages consumed
-            var userId = UUID.randomUUID().toString();
+        var userId = UUID.randomUUID().toString();
 
-            //create a message
-            final String value =  userId + ",54645, 2021";
-            String email = "Thank you for your order, your has been proccessed!";
+        //create a message
+        final String value = userId + ",54645, 2021";
 
-            //create a producer record within a topic, or simply writing a message in a topic
-            final ProducerRecord<String, String> record = new ProducerRecord<>("JAVA-TOPIC", value, value);
+        final var kafkaDispatcher = new KafkaDispatcher();
+        kafkaDispatcher.send("JAVA-TOPIC", value, value);
 
-            //create a producer record within a topic, or simply writing a message in a topic
-            final ProducerRecord<String, String> emailRecord = new ProducerRecord<>("ECOMMERCE-EMAIL", email, email);
-
-            //send a message to the topic
-            final Callback callback = (data, exception) -> {
-
-                if (Objects.nonNull(exception)) {
-                    exception.printStackTrace();
-                    return;
-                }
-
-                //the message sent
-                System.out.println("mensagem enviada com sucesso para o topico " + data.topic() + " offset " + data.offset() + " na particao " + data.partition() + " timestamp " + data.timestamp());
-
-            };
-
-            kafkaProducer.send(record, callback).get();//get will return a result in the future
-            kafkaProducer.send(emailRecord, callback).get();//get will return a result in the future
-        }
-    }
-
-    private static Properties properties() {
-        final var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        var email = "Thank you for your order, your has been proccessed!";
+        kafkaDispatcher.send("ECOMMERCE-EMAIL", email, email);
 
 
-        return properties;
     }
 }
